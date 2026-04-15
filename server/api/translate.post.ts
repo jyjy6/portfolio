fbe0666e2e1e3babaf2c596e12f857b326562247
jy <jungjuneyoung6@gmail.com>
2026-04-15 14:45:46 +0900

    refactor:api model changed : gemini-flash-llight

--- server/api/translate.post.ts
+++ server/api/translate.post.ts
@@ -7,7 +7,7 @@ ${text}`;
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${config.NUXT_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${config.NUXT_API_KEY}`,
       {
         method: "POST",
         headers: {
